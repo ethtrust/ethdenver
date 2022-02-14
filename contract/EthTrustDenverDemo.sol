42b247091aadab67356b0943dd9f128566f371a1
Steven J Schroeder <me@steven-schroeder.com>
2022-02-14 14:02:06 -0700

    Add First Pass of LightEmUp Solidity Contract

---
+++ contract/EthTrustDenverDemo.sol
@@ -0,0 +1,38 @@
+//SPDX-License-Identifier: ISC
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract EthTrustDenverDemo {
+    address public owner;
+    mapping(address => bool) public lightEmUp;
+
+    event ToggleOn(
+        address indexed _from
+    );
+
+    event ToggleOff(
+        address indexed _from
+    );
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function toggleOff() public {
+        require(lightEmUp[msg.sender] == true);
+        lightEmUp[msg.sender] = false;
+        emit ToggleOff(msg.sender);
+    }
+
+    function toggleOn() public {
+        require(lightEmUp[msg.sender] == false);
+        lightEmUp[msg.sender] = true;
+        emit ToggleOn(msg.sender);
+    }
+
+    function getLightState() public view returns (bool) {
+        return lightEmUp[msg.sender];
+    }
+}
+
