3b33e0840ff48a5c7b8702ec1d29a1406b38a480
Steven J Schroeder <me@steven-schroeder.com>
2022-02-14 20:27:24 -0700

    Include node package info for PoeListener.

--- poe-listener/poeListener.js
+++ poe-listener/poeListener.js
@@ -1,25 +1,25 @@
 const axios = require('axios');
 const Web3 = require('web3');
 
-const listenerConfig = require('./config/listenerConfig.json');
-const {poeAPIURL, poeAPIPort, web3ProviderURL, web3ProviderPort, contractAddress, contractABI} = listenerConfig;
+const listenerConfig = require('./config/poeListenerConfig.json');
+const { poeAPIURL, poeAPIPort, web3ProviderURL, web3ProviderPort, contractAddress, contractABI } = listenerConfig;
 
 const web3 = new Web3('ws://' + web3ProviderURL + ':' + web3ProviderPort);
 web3.eth.net.isListening()
 	.then((isUp) => {
-            console.log('Web3 is Up: ' + isUp);
-            if(isUp) {
-                listenForEvents();
-            } else {
-                console.error('Web3 is not up. Shutting down.');
-                process.exit(1);
-            }
-        }).catch((web3Err) => {
-            console.log('Web3 failed to Connect. Shutting down.\n' + web3Err);
-            process.exit(1);
-        });
+		if (isUp) {
+			console.log('Web3 is Up');
+			listenForEvents();
+		} else {
+			console.error('Web3 is not up. Shutting down.');
+			process.exit(1);
+		}
+	}).catch((web3Err) => {
+		console.log('Web3 failed to Connect. Shutting down.\n' + web3Err);
+		process.exit(1);
+	});
 
-async function listenForEvents(){
+async function listenForEvents() {
 	// Grab the Web3 Instance of the Contract
 	const lightEmUpContract = new web3.eth.Contract(contractABI, contractAddress);
 
@@ -27,32 +27,29 @@ async function listenForEvents(){
 	//   Catch the event and invoke a REST call to toggle the POE on.
 	lightEmUpContract.events.ToggleOn()
 		.on('data', (event) => {
-				console.log("Toggle On Caught");
-				console.log(event);
-				axios.get('http://' + poeAPIURL + ':' + poeAPIPort + '/on')
+			console.log("Toggle On Caught");
+			axios.get('http://' + poeAPIURL + ':' + poeAPIPort + '/on')
 				.then(response => {
-					console.log(response.data);
-					})
+					// Do Something
+				})
 				.catch(error => {
 					console.log(error);
-					});
-				})
-	.on('error', console.error);
+				});
+		})
+		.on('error', console.error);
 
 	// Handle the Toggle Off Event
 	//   Catch the event and invoke a REST call to toggle the POE off.
 	lightEmUpContract.events.ToggleOff()
 		.on('data', (event) => {
-				console.log("Toggle Off Caught");
-				console.log(event);
-				axios.get('http://' + poeAPIURL + ':' + poeAPIPort + '/off')
+			console.log("Toggle Off Caught");
+			axios.get('http://' + poeAPIURL + ':' + poeAPIPort + '/off')
 				.then(response => {
-					console.log(response.data);
-					})
+					// Do Something
+				})
 				.catch(error => {
 					console.log(error);
-					});
-				})
-	.on('error', console.error);
+				});
+		})
+		.on('error', console.error);
 }
-
