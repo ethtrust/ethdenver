cde165a32a431321691288dd0eebe76cc5e013f6
Ari Lerner <me@ari.io>
2022-02-17 18:11:50 -0500

    Added off

---
+++ client-app/src/pages/api/v1/off.ts
@@ -0,0 +1,34 @@
+import { createStatefulRouter } from "../../../utils";
+export default async function checkIsOn(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const router = createStatefulRouter();
+  const sessionCookie = req.cookies["session-cookie"];
+  try {
+    if (!sessionCookie || sessionCookie.length === 0) {
+      const getLoginResponse = await router.getLogin();
+      const handledResponse = await router.handleGetLogin(getLoginResponse);
+      const postLoginResponse = await router.postLogin();
+      const handledLoginResponse = await router.handlePostLogin(
+        postLoginResponse
+      );
+      res.cookie("session-cookie", handledLoginResponse.apiState.sessionCookie);
+      const getPoEPortConfigResp = await router.getPoEPortConfig();
+      await router.handleGetPoEPortConfig(getPoEPortConfigResp);
+
+      const postPoEPortConfigResp = await router.postPoEPortConfig(0);
+      await router.handlePostPoEPortConfig(postPoEPortConfigResp);
+    } else {
+      console.log("Session Cookie Stored");
+      const getPoEPortConfigResp = await router.getPoEPortConfig();
+      await router.handleGetPoEPortConfig(getPoEPortConfigResp);
+      const postPoEPortConfigResp = await router.postPoEPortConfig(0);
+      await router.handlePostPoEPortConfig(postPoEPortConfigResp);
+    }
+    return res.status(200).json({ success: true, currentPoEState: "0" });
+  } catch (err) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+}
